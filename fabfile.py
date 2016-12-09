5575222fb268816dea1d11bbbc74f194ae439a0b
Daniel Perez <daniel@claudetech.com>
2016-12-10 00:05:28 +0900

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,8 @@ from fabric.api import env, run, cd, local, lcd
 CODE_DIR  = '/home/videalize/videalize-video-processor'
 DEPLOY_DIR = '../deployment'
 BRANCH = 'master'
+PYTHON = '{0}/venv/bin/python'.format(CODE_DIR)
+PIP = '{0}/venv/bin/pip'.format(CODE_DIR)
 
 def fetch_hosts():
     with lcd(DEPLOY_DIR):
@@ -22,8 +24,11 @@ env.use_ssh_config = True
 def deploy():
     with cd(CODE_DIR):
         run("GIT_SSH_COMMAND='ssh -i ~/.ssh/video_processor_deploy' git fetch")
-        sha = run("git rev-parse {0}".format(BRANCH))
+        sha = run("git rev-parse origin/{0}".format(BRANCH))
         run("git checkout {0}".format(sha))
+
+        run("{0} install -r requirements.txt".format(PIP))
+        run("{0} setup.py install".format(PYTHON))
 
         pid = run("cat tmp/video_processor.pid")
         run("kill -SIGINT {0}".format(pid))
