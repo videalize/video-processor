644073f69cfd9eac78e5a2b7f26c936bb52dc592
taniTk <tanitk91@gmail.com>
2016-12-09 11:59:23 +0900

    カットする点の計算方法にヒストグラムの手法を追加

--- videalize/processor/SoundProcessor.py
+++ videalize/processor/SoundProcessor.py
@@ -10,11 +10,14 @@ class SoundProcessor:
     音量周りの処理
     '''
 
-    def __init__(self, filename, blocksize=44100):
+    def __init__(self, filename, blocksize=44100, method='HISTGRAM'):
         self.filename = filename
         self.blocksize = blocksize
         self.wav = self.load_volume_wav_per_blocks(blocksize)
-        self.filtered_wav = self.cut_volume_by_median()
+        if method == 'MEDIAN':
+            self.filtered_wav = self.cut_volume_by_median()
+        else:
+            self.filtered_wav = self.cut_volume_by_histgram()
 
     def load_wav(self):
         '''
@@ -38,7 +41,48 @@ class SoundProcessor:
         med = np.median(self.wav)
         return [w if w >= med else 0 for w in self.wav]
 
-    def make_cut_points_json(self):
+    def cut_volume_by_histgram(self):
+        '''
+        ヒストグラムを二値化することで，境界を計算
+        '''
+        bins = 1000
+        self.hist, self.bins = np.histogram(self.wav, bins)
+        threshould = self.otsu_threshould()
+        return [w if w >= threshould else 0 for w in self.wav]
+
+    def otsu_threshould(self):
+        '''
+        大津の手法によるヒストグラムの二値化
+        '''
+        mean = np.mean(self.wav)
+        max_idx = -1
+        max_s = 0
+
+        for t in range(1, len(self.bins)):
+            left = self.hist[:t]
+            right = self.hist[t:]
+
+            var_l = np.var(left)
+            mean_l = np.mean(left)
+            n_l = np.sum(left)
+            var_r = np.var(right)
+            mean_r = np.mean(right)
+            n_r = np.sum(right)
+
+            inner_var = (n_l*var_l + n_r*var_r) / (n_l + n_r)
+            between_var = (n_l * ((mean_l - mean)**2) + n_r * ((mean_r - mean)**2)) / (n_l + n_r)
+
+            s = between_var / inner_var
+            if s >= max_s:
+                max_idx = t
+                max_s = s
+
+        return self.bins[max_idx]
+
+    def make_cut_points(self):
+        '''
+        音量から切り出し点を計算
+        '''
         cut_points = []
         start_idx = -1
         end_idx = -1
@@ -60,6 +104,5 @@ class SoundProcessor:
             point = {"start": start_idx * self.blocksize, "end": (end_idx+1) * self.blocksize - 1}
             cut_points.append(point)
 
-        print(json.dumps({"cut_points": cut_points}))
-
-        return json.dumps({"cut_points": cut_points})
+        # return json.dumps({"cut_points": cut_points})
+        return cut_points
