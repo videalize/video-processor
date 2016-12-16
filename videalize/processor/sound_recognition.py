64035e683a893fc7c4b69247d963d1e77989abb4
Daniel Perez <daniel@claudetech.com>
2016-12-16 16:57:16 +0900

    Use speech recognition fork to include timestamps

--- videalize/processor/sound_recognition.py
+++ videalize/processor/sound_recognition.py
@@ -7,11 +7,18 @@ class SoundRecognition:
         self.username = settings.IBM_USERNAME
         self.password = settings.IBM_PASSWORD
 
-    def recognize(self,filename):
+    def recognize(self, filename):
         r = sr.Recognizer()
         with sr.AudioFile(filename) as source:
             audio = r.record(source)
-        return r.recognize_ibm(audio,username = self.username,password = self.password,language = "ja-JP",show_all = True)
+        return r.recognize_ibm(
+            audio,
+            username=self.username,
+            password=self.password,
+            language="ja-JP",
+            show_all=True,
+            timestamps=True
+        )
 
 if __name__ == '__main__':
     vr = SoundRecognition()
